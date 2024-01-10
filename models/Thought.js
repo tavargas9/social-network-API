80a840b218118bde5c4a48d52e2ca5ab60894f17
Tomas Vargas <tavargas9@gmail.com>
2024-01-09 20:14:24 -0700

    edit User schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,9 @@
-const { Schema, Types } = require('mongoose');
+const { Schema, Types, model } = require('mongoose');
 
+const thoughtSchema = new Schema({
+
+})
+
+const Thought = model('thought', thoughtSchema);
 
 module.exports = thoughtSchema;
