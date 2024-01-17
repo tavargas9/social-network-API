a5dba93598f45fc75fb7b917db776da993498eb3
Tomas Vargas <tavargas9@gmail.com>
2024-01-17 14:01:22 -0700

    fix getters

--- models/Reaction.js
+++ models/Reaction.js
@@ -20,7 +20,7 @@ const reactionSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now,
-            get: () => formatDate(Date.now()),
+            get: createdAt => formatDate(createdAt),
         },
     },
     {
--- models/Thought.js
+++ models/Thought.js
@@ -13,7 +13,7 @@ const thoughtSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      get: () => formatDate(Date.now()),
+      get: createdAt => formatDate(createdAt),
     },
     username: {
       type: String,
