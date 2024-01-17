35052c7ac441404284110e3b933d9a799a87159b
Tomas Vargas <tavargas9@gmail.com>
2024-01-17 12:52:40 -0700

    add User model and virtuals

--- models/User.js
+++ models/User.js
@@ -2,8 +2,43 @@ const { Schema, Types, model } = require('mongoose');
 
 // Schema to create a user model
 const userSchema = new Schema(
-
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Not a valid email address!"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
 
 const User = model('user', userSchema);
 
